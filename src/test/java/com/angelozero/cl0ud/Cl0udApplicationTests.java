6bbab69a20d143761955d89e017a41bc4654464e
Angelo Zero <angelo.akm@gmail.com>
2023-01-02 20:56:58 -0300

    initial commit

---
+++ src/test/java/com/angelozero/cl0ud/Cl0udApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelozero.cl0ud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl0udApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
