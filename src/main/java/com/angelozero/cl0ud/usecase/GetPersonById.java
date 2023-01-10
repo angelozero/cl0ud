1b8aab3fd71193939406119428f2996635ce29f2
Angelo Zero <angelo.akm@gmail.com>
2023-01-09 22:46:21 -0300

    creatin a simple crud

--- src/main/java/com/angelozero/cl0ud/usecase/GetPersonById.java
+++ src/main/java/com/angelozero/cl0ud/usecase/GetPersonById.java
@@ -2,13 +2,16 @@ package com.angelozero.cl0ud.usecase;
 
 import com.angelozero.cl0ud.usecase.model.Person;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @AllArgsConstructor
 public class GetPersonById {
 
     public Person execute(int id) {
+        log.info("[CLOUD-APP] - Get a person by id: {}", id);
         return id == 32 ? generatePerson(32, "Angelo") : generatePerson(12, "Jake");
     }
 
@@ -19,3 +22,4 @@ public class GetPersonById {
                 .build();
     }
 }
+
--- src/main/java/com/angelozero/cl0ud/usecase/GetPersons.java
+++ src/main/java/com/angelozero/cl0ud/usecase/GetPersons.java
@@ -2,16 +2,19 @@ package com.angelozero.cl0ud.usecase;
 
 import com.angelozero.cl0ud.usecase.model.Person;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class GetPersons {
 
     public List<Person> execute() {
+        log.info("[CLOUD-APP] - Get a list of persons");
         return Arrays.asList(
                 Person.builder()
                         .age(32)
