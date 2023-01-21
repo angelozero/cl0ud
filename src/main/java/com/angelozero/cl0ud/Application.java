a915fbe2d90e55ed1b6012d6931148305a97d720
Angelo Zero <angelo.akm@gmail.com>
2023-01-20 22:09:41 -0300

    removing scan annotations

--- src/main/java/com/angelozero/cl0ud/Application.java
+++ src/main/java/com/angelozero/cl0ud/Application.java
@@ -2,14 +2,7 @@ package com.angelozero.cl0ud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableJpaRepositories("com.angelozero.cl0ud.*")
-@ComponentScan("com.angelozero.cl0ud.*")
-@EntityScan("com.angelozero.cl0ud.*")
 @SpringBootApplication
 public class Application {
 
