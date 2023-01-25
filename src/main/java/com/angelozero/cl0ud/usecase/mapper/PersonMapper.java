47766636a4e138a993f4ffe1b235750835996a35
Angelo Zero <angelo.akm@gmail.com>
2023-01-24 22:12:59 -0300

    creating the mapper

--- src/main/java/com/angelozero/cl0ud/usecase/mapper/PersonMapper.java
+++ src/main/java/com/angelozero/cl0ud/usecase/mapper/PersonMapper.java
@@ -1,2 +1,12 @@
-package com.angelozero.cl0ud.usecase.mapper;public interface PersonMapper {
+package com.angelozero.cl0ud.usecase.mapper;
+
+import com.angelozero.cl0ud.gateway.postgressql.entity.PersonEntity;
+import com.angelozero.cl0ud.usecase.model.Person;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PersonMapper {
+    Person toModel(PersonEntity personEntity);
+
+    PersonEntity toEntity(Person person);
 }
