21bbc130ff3562c0fea70ee9ee390d55aa7dea46
Angelo Zero <angelo.akm@gmail.com>
2023-02-08 22:51:50 -0300

    refac gitignore file

--- create.sql
+++ create.sql
@@ -1,4 +1,2 @@
 create table "cl0ud"."person" ("id" bigint not null, "age" integer, "name" varchar(255), primary key ("id"));
-create sequence "cl0ud"."person_seq" start with 1 increment by 50;
-create table "cl0ud"."person" ("id" bigint not null, "age" integer, "name" varchar(255), primary key ("id"));
 create sequence "person_seq" start with 1 increment by 50;
