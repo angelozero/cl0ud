28d44f165400d4c15401a1e18d538ff5ecd801da
Angelo Zero <angelo.akm@gmail.com>
2023-02-13 21:36:32 -0300

    adding local profile file

--- create.sql
+++ create.sql
@@ -50,3 +50,5 @@ create table cl0ud.person (id bigint not null, age integer, name varchar(255), p
 create sequence person_seq start with 1 increment by 50;
 create sequence person_seq start with 1 increment by 50;
 create table person (id bigint not null, age integer, name varchar(255), primary key (id));
+create sequence person_seq start with 1 increment by 50;
+create table person (id bigint not null, age integer, name varchar(255), primary key (id));
