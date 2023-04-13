24f868e5407569235bf11c6d8ddaf8d3b49921cf
Angelo Zero <angelo.akm@gmail.com>
2023-04-12 21:32:14 -0300

    refac the log message

---
+++ src/main/java/com/angelozero/cl0ud/usecase/utils/LogMessages.java
@@ -0,0 +1,5 @@
+package com.angelozero.cl0ud.usecase.utils;
+
+public class LogMessages {
+    public static final  String LOG_INFO_CREATING_PERSON = "\n[CREATING_PERSON] - Creating a person: {}\n";
+}
