b106dd9dd4f22c49eab775e8df612a456855d172
Angelo Zero <angelo.akm@gmail.com>
2023-04-13 21:35:25 -0300

    refac the log message

--- src/main/java/com/angelozero/cl0ud/usecase/utils/LogMessages.java
+++ src/main/java/com/angelozero/cl0ud/usecase/utils/LogMessages.java
@@ -1,5 +1,20 @@
 package com.angelozero.cl0ud.usecase.utils;
 
 public class LogMessages {
-    public static final  String LOG_INFO_CREATING_PERSON = "\n[CREATING_PERSON] - Creating a person: {}\n";
+
+    /**
+     * LOG INFO
+     */
+    public static final String LOG_INFO_CREATE_PERSON = "\n[CREATING_PERSON] - Creating a person: {}\n";
+
+    /**
+     * LOG ERROR
+     */
+    public static final String LOG_ERROR_CREATE_PERSON = "\n[ERROR] - Error to create a person\n";
+
+    /**
+     * EXCEPTION
+     */
+    public static final String EXCEPTION_PERSON_DATA_IS_NULL = "Person Data is null";
+    public static final String EXCEPTION_TO_CREATE_A_PERSON = "Person Data is null";
 }
