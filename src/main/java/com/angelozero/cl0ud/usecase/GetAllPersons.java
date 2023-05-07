f6d88b1bd31e7e33b93c3ad075cea76cfd33e361
Angelo Zero <angelo.akm@gmail.com>
2023-05-06 22:00:39 -0300

    refac the log message

--- src/main/java/com/angelozero/cl0ud/usecase/GetAllPersons.java
+++ src/main/java/com/angelozero/cl0ud/usecase/GetAllPersons.java
@@ -4,6 +4,7 @@ import com.angelozero.cl0ud.exception.person.GetAllPersonsException;
 import com.angelozero.cl0ud.gateway.DataBaseGateway;
 import com.angelozero.cl0ud.usecase.mapper.PersonMapper;
 import com.angelozero.cl0ud.usecase.model.Person;
+import com.angelozero.cl0ud.usecase.utils.LogMessage;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,13 +20,13 @@ public class GetAllPersons {
     private final PersonMapper personMapper;
 
     public List<Person> execute() {
-        log.info("[GET_ALL_PERSONS] - Get a list of persons");
+        log.info(LogMessage.LOG_INFO_GET_ALL_PERSONS);
 
         try {
             return personMapper.toModelList(dataBaseGateway.getAllPersonsEntity());
 
         } catch (Exception ex) {
-            log.error("\n[ERROR] - Error to get all persons\n");
+            log.error(LogMessage.LOG_ERROR_GET_ALL_PERSONS);
             throw new GetAllPersonsException("Error to get all persons: " + ex.getMessage());
         }
     }
