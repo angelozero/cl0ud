03b77f37e3bb58303c3293706801382deb886e5c
Angelo Zero <angelo.akm@gmail.com>
2023-05-25 21:14:34 -0300

    refac

--- src/main/java/com/angelozero/cl0ud/usecase/utils/LogMessage.java
+++ src/main/java/com/angelozero/cl0ud/usecase/utils/LogMessage.java
@@ -8,7 +8,7 @@ public class LogMessage {
     /**
      * DeletePersonById
      */
-    public static final String INFO_DELETE_PERSON_BY_ID = "\n[DELETE_PERSON_BY_ID] - Deleting a person by id: {}\n";
+    public static final String INFO_DELETE_PERSON_BY_IDs = "\n[DELETE_PERSON_BY_ID] - Deleting a person by id: {}\n";
     public static final String ERROR_DELETE_PERSON = "\n[ERROR] - Error to delete a person\n";
 
     /**
