a6fc31b6bf3d856ab1ddc01fef2ee35c815126a2
Angelo Zero <angelo.akm@gmail.com>
2023-08-31 23:58:41 -0300

    fixing some unit testes

--- src/test/java/com/angelozero/cl0ud/unit/auth_jwt/service/GenerateRefreshTokenTest.java
+++ src/test/java/com/angelozero/cl0ud/unit/auth_jwt/service/GenerateRefreshTokenTest.java
@@ -1,13 +1,10 @@
 package com.angelozero.cl0ud.unit.auth_jwt.service;
 
-import com.angelozero.cl0ud.auth_jwt.gateway.TokenGateway;
-import com.angelozero.cl0ud.auth_jwt.gateway.entity.RefreshTokenEntity;
-import com.angelozero.cl0ud.auth_jwt.gateway.entity.UserEntity;
+import com.angelozero.cl0ud.auth_jwt.service.FindUserByEmail;
 import com.angelozero.cl0ud.auth_jwt.service.GenerateRefreshToken;
+import com.angelozero.cl0ud.auth_jwt.service.SaveRefreshToken;
 import com.angelozero.cl0ud.auth_jwt.service.dao.TokenRefreshed;
 import com.angelozero.cl0ud.auth_jwt.service.dao.User;
-import com.angelozero.cl0ud.auth_jwt.service.mapper.RefreshTokenMapper;
-import com.angelozero.cl0ud.auth_jwt.service.mapper.UserMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,12 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.UUID;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class GenerateRefreshTokenTest {
@@ -29,28 +24,26 @@ public class GenerateRefreshTokenTest {
     private GenerateRefreshToken generateRefreshToken;
 
     @Mock
-    private TokenGateway tokenGateway;
+    private FindUserByEmail findUserByEmail;
 
     @Mock
-    private UserMapper userMapper;
-
-    @Mock
-    private RefreshTokenMapper refreshTokenMapper;
+    private SaveRefreshToken saveRefreshToken;
 
 
     @DisplayName("Should generate refresh token with success")
     @Test
     void testShouldGenerateRefreshTokenWithSuccess() {
 
-        when(tokenGateway.findUserByEmail(anyString()))
-                .thenReturn(UserEntity.builder().fullname("user test").build());
-        when(userMapper.toModel(any(UserEntity.class))).thenReturn(User.builder().fullname("user test").build());
-        when(refreshTokenMapper.toEntity(any(TokenRefreshed.class))).thenReturn(RefreshTokenEntity.builder().token(UUID.randomUUID().toString()).build());
-        when(tokenGateway.save(any(RefreshTokenEntity.class))).thenReturn(RefreshTokenEntity.builder().build());
-        when(refreshTokenMapper.toModel(any(RefreshTokenEntity.class))).thenReturn(TokenRefreshed.builder().token("token test").build());
+        when(findUserByEmail.execute(anyString()))
+                .thenReturn(User.builder().fullname("user test").build());
+        when(saveRefreshToken.execute(any(TokenRefreshed.class)))
+                .thenReturn(TokenRefreshed.builder().token("token test").build());
 
         TokenRefreshed response = generateRefreshToken.execute("email");
 
         assertNotNull(response);
+
+        verify(findUserByEmail, times(1)).execute(anyString());
+        verify(saveRefreshToken, times(1)).execute(any(TokenRefreshed.class));
     }
 }
